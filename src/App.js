3f30c27d933eb93bd86e77b4f431b35a1e4cd1dd
tadeotoby <165961200+tadeotoby@users.noreply.github.com>
2024-04-22 14:05:10 -0600

    cambios

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function Board({ xIsNext, squares, onPlay }) {
     if (xIsNext) {
       nextSquares[i] = 'X';
     } else {
-      nextSquares[i] = '';
+      nextSquares[i] = '0';
     }
     onPlay(nextSquares);
   }
